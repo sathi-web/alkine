98a7710559b448df6aa2f0a77b585b86d65133fe
sathi <mahabubalomlanju99@gmail.com>
2023-11-04 20:36:47 +0600

    alkine

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.review-wrapper').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+     
+      centerMode: true,
+      dots:true,
+      arrows:false,
+    });
+  });
